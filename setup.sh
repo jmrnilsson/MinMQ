56f86c58f5898ab5fabf3fbaa6e29ef455d865ea
jmrnilsson <jmrnilsson@users.noreply.github.com>
2019-11-24 07:01:50 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 mkdir -p /data
-sudo docker volume create --name=fasterdata --driver local --opt o=size=1200m --opt device="/data/docker-hlog" --opt type=tmpfs
+docker volume create --name=fasterdata --driver local --opt o=size=1200m --opt device="/data/docker-hlog" --opt type=tmpfs
+ 
