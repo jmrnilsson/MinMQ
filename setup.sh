dad541e958ce71eaea51d8531d1d700228a92aec
jmrnilsson <martin.jr.nilsson@gmail.com>
2019-11-24 06:34:56 +0100

    11

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env pwsh
-mkdir /data
-docker volume create --name=fasterdata --driver local --opt o=size=1200m --opt device="/data/docker-hlog" --opt type=tmpfs
+#!/usr/bin/env bash
+mkdir -p /data
+sudo docker volume create --name=fasterdata --driver local --opt o=size=1200m --opt device="/data/docker-hlog" --opt type=tmpfs
