c7b3c317ef4df68e7aa15cc6a271ee46add2d559
jmrnilsson <martin.jr.nilsson@gmail.com>
2019-11-25 02:49:44 +0100

    working 266638

--- service-kestrel/Service-Kestrel/Service-Kestrel/RequestHandlers/FasterHandler.cs
+++ service-kestrel/Service-Kestrel/Service-Kestrel/RequestHandlers/FasterHandler.cs
@@ -12,34 +12,34 @@ namespace Service_Kestrel.RequestHandlers
 {
 	public static class FasterHttpHandler
 	{
-		private static SemaphoreSlim requestThrottling { get; set; } = new SemaphoreSlim(3, 3);
+		/// private static SemaphoreSlim requestThrottling { get; set; } = new SemaphoreSlim(8, 8);
 
 		public static async Task HandleRequest(HttpContext context)
 		{
-			await requestThrottling.WaitAsync();
+			// await requestThrottling.WaitAsync();
 
-			try
-			{
+			//try
+			//{
 
-			using (StreamReader reader = new StreamReader(context.Request.Body))
-			{
-				var body = await reader.ReadToEndAsync();
-				var bytes = Encoding.ASCII.GetBytes(body);
-				CancellationTokenSource cts = new CancellationTokenSource();
-				long address = await FasterWriter.Instance.Value.EnqueueAsync(bytes, cts.Token);
-				await FasterWriter.Instance.Value.CommitAsync(cts.Token);
-				await FasterWriter.Instance.Value.WaitForCommitAsync(address, cts.Token);
-				context.Response.StatusCode = 201;
-				await context.Response.WriteAsync("Created");
-			}
-			}
-			catch (Exception e) when (e.Message.Contains("Unable to add item to list"))
-			{
-				context.Response.StatusCode = 503;
-				await context.Response.WriteAsync("Busy");
-			}
+				using (StreamReader reader = new StreamReader(context.Request.Body))
+				{
+					var body = await reader.ReadToEndAsync();
+					var bytes = Encoding.ASCII.GetBytes(body);
+					CancellationTokenSource cts = new CancellationTokenSource();
+					long address = await FasterWriter.Instance.Value.EnqueueAsync(bytes, cts.Token);
+					// await FasterWriter.Instance.Value.CommitAsync(cts.Token);
+					await FasterWriter.Instance.Value.WaitForCommitAsync(address, cts.Token);
+					context.Response.StatusCode = 201;
+					await context.Response.WriteAsync("Created");
+				}
+			//}
+			//catch (Exception e) when (e.Message.Contains("Unable to add item to list"))
+			//{
+			//	context.Response.StatusCode = 503;
+			//	await context.Response.WriteAsync("Busy");
+			//}
 
-			requestThrottling.Release();
+			/// requestThrottling.Release();
 
 		}
 	}
