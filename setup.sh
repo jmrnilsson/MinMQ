afa735ab7e7a4bb775921ca77175124a073243c6
jmrnilsson <martin.jr.nilsson@gmail.com>
2019-12-05 01:56:01 +0100

    fix volume name .sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 mkdir -p /data
-docker volume create --name=fasterdata --driver local --opt o=size=1200m --opt device="/data/docker-hlog" --opt type=tmpfs
+docker volume create --name=postgresdata --driver local --opt o=size=1200m --opt device="/data/docker-hlog" --opt type=tmpfs
  
